aaf9b549df8c4ea1c5b494dafb55da4ce2c69db6
Anshika-tech <srivastavanshika10@gmail.com>
2022-01-22 13:49:56 +0530

    Recursion

---
+++ src/Recursion.java
@@ -0,0 +1,16 @@
+//calculate x^n x to the power of n by recursive call.
+import java.util.Scanner;
+public class Recursion {
+    public static int power(int x,int n){
+        if(n==1){
+            return x;
+        }
+        return x*power(x,n-1);
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        int n=sc.nextInt();
+        System.out.println(power(x,n));
+    }
+}
