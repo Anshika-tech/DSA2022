9ad669018a6bfb162f6b6c6167ef009aa0305f92
Anshika-tech <srivastavanshika10@gmail.com>
2022-01-31 19:32:35 +0530

    Linked list

--- src/Linkedlist.java
+++ src/Linkedlist.java
@@ -1,5 +1,5 @@
 //Linked list traversal
-public class Linkedlist {
+/*public class Linkedlist {
     Node head;
     static class Node{
         int data;
@@ -24,5 +24,45 @@ public class Linkedlist {
        l1.head.next=n2;
       n2.next=n3;
       l1.display();
+    }
+}*/
+//Insertion of a Node
+//1. At the front of the linked list.
+public class Linkedlist{
+    Node head;
+    static class Node{
+        int data;
+        Node next;
+        Node(int d){
+            data=d;
+            next=null;
+        }
+    }
+    public void display(){
+        Node n=head;
+        while(n!=null){
+            System.out.print(n.data+" ");
+            n=n.next;
+        }
+        System.out.println(" ");
+    }
+    public void insert(int new_data){
+        Node new_node=new Node(new_data);
+        new_node.next=head;
+        head=new_node;
+    }
+    public static void main(String[] args) {
+         Linkedlist l1=new Linkedlist();
+         l1.head=new Node(5);
+         Node n2=new Node(6);
+         Node n3=new Node(7);
+         l1.head.next=n2;
+         n2.next=n3;
+        System.out.println("before insertion ");
+         l1.display();
+        System.out.println("after insertion of a node at the front");
+         l1.insert(4);
+         l1.display();
+
     }
 }
