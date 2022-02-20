41c5f3795a7fcc44619babaa4ba8997c798a32b8
Anshika-tech <srivastavanshika10@gmail.com>
2022-02-20 11:28:29 +0530

    stack by linked list

--- src/Stack.java
+++ src/Stack.java
@@ -1,5 +1,5 @@
 //implement stack by using an array
-public class Stack {
+/*public class Stack {
    final int Max=1000;
    int top;
    int a[]=new int[Max];
@@ -56,5 +56,69 @@ public class Stack {
         System.out.println("Element present in Stack;");
         s.print();
 
+    }
+}*/
+// implementation of stack by using linked list.
+class Node{
+    int data;
+    Node next;
+    public Node(int d1){
+        this.data=d1;
+    }
+}
+public class Stack{
+    Node root;
+    boolean isEmpty(){
+        if(root==null)
+            return true;
+        else
+            return false;
+    }
+    public void push(int data){
+        Node new_node=new Node(data);
+        if(root==null){
+            root=new_node;
+        }else{
+            Node temp=root;
+            root=new_node;
+            new_node.next=temp;
+        }
+        System.out.println(data+ "pushed to stack");
+    }
+    public int pop(){
+        int popped=Integer.MIN_VALUE;
+        if(root==null)
+            System.out.println("Stack is Empty");
+        else
+        {
+            popped=root.data;
+            root=root.next;
+        }
+        return popped;
+    }
+    public int peek(){
+        if(root==null){
+            System.out.println("Stack is empty");
+            return Integer.MIN_VALUE;
+        }
+        else
+            return root.data;
+    }
+    void traverse(){
+        Node p=root;
+        while(p!=null){
+            System.out.println(p.data+ " ");
+            p=p.next;
+        }
+    }
+    public static void main(String[] args) {
+        Stack obj=new Stack();
+        System.out.println(obj.isEmpty());
+         obj.push(10);
+         obj.push(20);
+         obj.push(30);
+        System.out.println(obj.pop()+" popped from stack");
+        System.out.println("Top element is "+obj.peek());
+        obj.traverse();
     }
 }
