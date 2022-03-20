cb4e00dc3f38225251ae500794790583278d96c0
Anshika-tech <srivastavanshika10@gmail.com>
2022-03-20 14:55:56 +0530

    binary search

--- src/Search.java
+++ src/Search.java
@@ -31,7 +31,7 @@ public class Search {
         }
 
     }
-}*/
+}
 //2. by iteration.
 import java.util.Scanner;
 public class Search{
@@ -62,5 +62,44 @@ public class Search{
         }
         else
             System.out.println("element found at index "+result);
+    }
+}*/
+//find first and last position of an element.
+import java.util.Scanner;
+public class Search{
+    public static int first(int[] a,int lo,int hi,int x){
+        if(hi>=lo){
+            int mid=lo+(hi-lo)/2;
+            if((mid==0 || x>a[mid-1])&&a[mid]==x){
+                return mid;
+            }
+            else if(x>a[mid]){
+                return first(a,(mid+1),hi,x);
+            }
+            else
+                return first(a,lo,(mid-1),x);
+        }
+        return -1;
+    }
+    public static int last(int[] a,int lo,int hi,int x,int n){
+        if(hi>=lo){
+            int mid=lo+(hi-lo)/2;
+            if((mid==n-1 || x<a[mid+1])&&a[mid]==x){
+                return mid;
+            }
+            else if(x<a[mid])
+                return last(a,lo,(mid-1),x,n);
+            else
+                return last(a,(mid+1),hi,x,n);
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int[] a={1,2,3,3,3,3,4,5,8,8};
+        int n=a.length;
+        int x=sc.nextInt();
+        System.out.println("first occurence = "+first(a,0,n-1,x));
+        System.out.println("last occurence ="+last(a,0,n-1,x,n));
     }
 }
