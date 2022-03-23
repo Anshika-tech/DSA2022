4eca55e12422330144468dc71fa4df324c0d9f0e
Anshika-tech <srivastavanshika10@gmail.com>
2022-03-23 23:26:52 +0530

    sorting by selection sort.

--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -1,5 +1,5 @@
 //time complexity of selection sort is O(n^2).
-public class SelectionSort {
+/*public class SelectionSort {
     static void sort(int[] a,int n){
         for(int i=0;i<=n-1;i++){
             for(int j=i+1;j<n;j++){
@@ -18,6 +18,29 @@ public class SelectionSort {
         sort(a,n);
         for(int value:a){
             System.out.print(value+" ");
+        }
+    }
+}*/
+//sort an array of string by selection sort.
+public class SelectionSort{
+    static void sort(String[] a,int n){
+         for(int i=0;i<n-1;i++){
+             for(int j=i+1;j<n;j++){
+                 if(a[j].compareTo(a[i])<0){
+                     String t=a[i];
+                     a[i]=a[j];
+                     a[j]=t;
+                 }
+
+             }
+         }
+    }
+    public static void main(String[] args) {
+        String[] a={"paper","true" ,"soap", "floppy", "flower"};
+        int n=a.length;
+        sort(a,n);
+        for(int i=0;i<n;i++){
+            System.out.print(a[i]+" ");
         }
     }
 }
