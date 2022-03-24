e193ac79bdcb13c70521349daa8fc2b8c442ae94
Anshika-tech <srivastavanshika10@gmail.com>
2022-03-24 20:08:53 +0530

    Bubble sort

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -1,6 +1,6 @@
 //worst time complexity of this sort method is O(n^2).
 //Best time complexity of bubble sort is O(n), when array is sorted.
-public class BubbleSort {
+/*public class BubbleSort {
     static void sort(int[] a,int n){
         for(int i=0;i<n-1;i++){
             for(int j=0;j<n-i-1;j++){
@@ -20,4 +20,31 @@ public class BubbleSort {
             System.out.print(value+" ");
         }
     }
+}*/
+//bubble sort by recursion.
+public class BubbleSort{
+     void sort(int[] a,int n){
+         if(n==1)
+             return;
+         for(int i=0;i<n-1;i++){
+             if(a[i]>a[i+1]){
+                 int t=a[i];
+                 a[i]=a[i+1];
+                 a[i+1]=t;
+             }
+         }
+         sort(a,n-1);
+     }
+    public static void main(String[] args) {
+        BubbleSort obj=new BubbleSort();
+        int[] a={5,6,3,2,8,7};
+        int n=a.length;
+        obj.sort(a,n);
+        for(int i=0;i<n;i++){
+            System.out.print(a[i]+" ");
+        }
+
+    }
 }
+
+
